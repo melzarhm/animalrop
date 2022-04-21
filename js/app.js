36e6d90171c2346db0a103cefac02fb09bd1779f
melzarhm <104119724+melzarhm@users.noreply.github.com>
2022-04-20 23:16:45 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+  $('.carrusel').slick({
+	arrows: true,
+  	dots: true,
+  	autoplay:true,
+  	infinite: true,
+  	slidesToShow: 3,
+  	slidesToScroll: 3
+  });
+
+
+  $('.banner').slick({
+	arrows: true,
+  	dots: false,
+  	autoplay:true,
+  	
+  });
+
